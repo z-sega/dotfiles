b307a1847be043f655b4129b0f0465491c10740b
Ayo Onipe <ayosemail@gmail.com>
2023-07-18 00:20:23 -0600

    Remove open-api-key, using .env file instead

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,4 +18,31 @@
  ;; If there is more than one, they won't work right.
  )
 
-(setq gptel-api-key "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")
+;; ------------------ UTILS TO DO THINGS
+;; ------------------ from .env:
+;; ------------------ load a variable
+(defun load-env-variable (variable-name)
+  "Load the value of VARIABLE-NAME from the .env file."
+  (interactive "sEnter the name of the variable: ")
+  (with-temp-buffer
+    (insert-file-contents ".env")
+    (goto-char (point-min))
+    (while (re-search-forward (concat "^" (regexp-quote variable-name) "=\\([^[:space:]]+\\)") nil t)
+      (setq temp-variable (match-string 1)))
+    (message "%s: %s" variable-name temp-variable)))
+
+;; ----------------- return a variable
+(defun grab-env (variable-name)
+  "Grab the value of VARIABLE-NAME from the .env file."
+  (insert-file-contents ".env")
+  (goto-char (point-min))
+  (while (re-search-forward (concat "^" (regexp-quote variable-name) "=\\([^[:space:]]+\\)") nil t)
+    match-string 1))
+
+	     
+;; ------------------ open-ai-api
+;; -------- using load-env ...
+;; (load-env-variable "OPEN_API_KEY")
+;; (setq gptel-api-key temp-variable)
+;; -------- using grab-env ...
+(setq gptel-api-key (grab-env "OPEN_API_KEY")
