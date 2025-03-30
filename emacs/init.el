0693b2a201d4a28a38ce4c78d6a05c1945f53526
Ayodeji Onipe <ayo@Ayodejis-MacBook-Pro.local>
2025-03-29 23:25:02 -0600

    Initial emacs configuration

---
+++ emacs/init.el
@@ -0,0 +1,12 @@
+;;(package-initialize)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#222327" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 152 :width normal :foundry "nil" :family "TX-02")))))
+
+
+(org-babel-load-file  "~/.emacs.d/mac.org")
+(org-babel-load-file  "~/.emacs.d/config.org")
