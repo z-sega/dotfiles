e0394bcefc53bd79ab7f284b834a4863855c7218
ayodeji_____ <ayosemail@gmail.com>
2025-04-08 13:46:38 -0600

    Add gptel deepseek as default

--- emacs/init.el
+++ emacs/init.el
@@ -11,7 +11,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#222327" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 152 :width normal :foundry "nil" :family "TX-02")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#222327" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 152 :width normal :foundry "nil" :family "TX-02"))))
+ '(eshell-prompt ((t (:foreground "#FFD866" :weight bold)))))
 
 
 (org-babel-load-file  "~/.emacs.d/mac.org")
