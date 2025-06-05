2e949ee33fd4f583f1e80c9a0834df950f61e61f
ayodeji_____ <ayosemail@gmail.com>
2025-06-05 01:21:07 -0400

    Resolve elpaca, org-babel and org versioning issue

--- emacs/init.el
+++ emacs/init.el
@@ -14,8 +14,63 @@
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#222327" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 152 :width normal :foundry "nil" :family "TX-02"))))
  '(eshell-prompt ((t (:foreground "#FFD866" :weight bold)))))
 
+;; Standard Elpaca Bootstrap
+(defvar elpaca-installer-version 0.11)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+                              :ref nil :depth 1 :inherit ignore
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+                              :build (:not elpaca--activate-package)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list 'load-path (if (file-exists-p build) build repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                  ,@(when-let* ((depth (plist-get order :depth)))
+                                                      (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                  ,(plist-get order :repo) ,repo))))
+                  ((zerop (call-process "git" nil buffer t "checkout"
+                                        (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
+                                        "--eval" "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn (message "%s" (buffer-string)) (kill-buffer buffer))
+          (error "%s" (with-current-buffer buffer (buffer-string))))
+      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+;; Install use-package support
+(elpaca elpaca-use-package
+  ;; Enable use-package :ensure support for Elpaca.
+  (elpaca-use-package-mode))
+
+(setq use-package-always-ensure t)
+;; -------- Elpaca done
+
+;; Use latest org-mode. Do this early, to use when loading config
+(use-package org
+  :ensure (:wait t)
+  :mode ("\\.org\\'" . org-mode))
+
+;; Load main emacs config.org
 (org-babel-load-file  "~/.emacs.d/mac.org")
 (org-babel-load-file  "~/.emacs.d/config.org")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,6 +89,13 @@
      (:name "drafts" :query "tag:draft" :key [100] :sort-order
 	    newest-first)
      (:name "all mail" :query "*" :key [97] :sort-order newest-first)))
+ '(safe-local-variable-values
+   '((python-shell-process-environment
+      "DJANGO_SETTINGS_MODULE=app.settings")
+     (python-shell-virtualenv-root . "./venv")
+     (python-shell-extra-pythonpaths ".")
+     (python-shell-process-environment
+      "DJANGO_SETTINGS_MODULE=project.settings")))
  '(scroll-bar-mode nil)
  '(standard-indent 2)
  '(tool-bar-mode nil)
